f24aa59027093708b6a580d9ac6da517dff69a88
Pranav Bahuguna <pranav.bahuguna@gmail.com>
2015-06-18 14:51:28 +0100

    Added DeltaX/DeltaY methods to Direction class

--- Assets/Scripts/Direction.cs
+++ Assets/Scripts/Direction.cs
@@ -11,4 +11,35 @@ public static class Extensions
 	{
 		return ((int)direction + 3) % 6; 
 	}
+
+	// Determines the horizontal displacement given by moving in the given direction
+	// by one unit.
+	public static int DeltaX(this Direction direction)
+	{
+		switch (direction) {
+			case (Direction.UP): 			return 0;
+			case (Direction.UP_RIGHT):		return 1;
+			case (Direction.DOWN_RIGHT):	return 1;
+			case (Direction.DOWN):			return 0;
+			case (Direction.DOWN_LEFT):		return -1;
+			case (Direction.UP_LEFT):		return -1;
+			default:						return 0;
+		}
+	}
+
+	// Determines the vertical displacement given by moving in the given direction
+	// by one unit. This takes an additional bool 'isColUpper' (whether a column of
+	// cells 'looks' higher than its neighbouring columns).
+	public static int DeltaY(this Direction direction, bool isColUpper)
+	{
+		switch (direction) {
+			case (Direction.UP): 			return 1;
+			case (Direction.UP_RIGHT):		return (isColUpper) ? 1 : 0;
+			case (Direction.DOWN_RIGHT):	return (isColUpper) ? -1 : 0;
+			case (Direction.DOWN):			return 1;
+			case (Direction.DOWN_LEFT):		return (isColUpper) ? -1 : 0;
+			case (Direction.UP_LEFT):		return (isColUpper) ? 1 : 0;
+			default:						return 0;
+		}
+	}
 }

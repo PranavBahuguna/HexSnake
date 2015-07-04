2d4e128d2931dc74a73ac6a0d16d4a01966b8d91
Pranav Bahuguna <pranav.bahuguna@gmail.com>
2015-07-04 15:40:08 +0100

    Added a pause menu, which can resume or exit game

--- Assets/Scripts/Game/PauseGame.cs
+++ Assets/Scripts/Game/PauseGame.cs
@@ -4,20 +4,28 @@ using System.Collections;
 /* Provides a method to allow the game to be paused on/off */
 public class PauseGame : MonoBehaviour
 {
-	public KeyCode pause;	// Pause key
-	private GameManager gm; // Accessor to GameManager script.
+	public KeyCode pause;			// Pause key
+	public GameObject pauseMenu;	// Accessor to the pause menu.
+	private GameManager gm; 		// Accessor to GameManager script.
 	
 	private void Start() 
 	{
 		gm = GetComponent<GameManager>();
+		pauseMenu.SetActive (false);
 	}
 	
 	private void Update() 
 	{
-		// Stops or allows the the gm script to be updated when
-		// pause key is pressed.
 		if (Input.GetKeyUp (pause)) {
-			gm.enabled = !gm.enabled;
+			Pause();
 		}
+	}
+
+	// Carries out the various actions involved when pausing/unpausing
+	// the game.
+	public void Pause()
+	{
+		gm.enabled = !gm.enabled;
+		pauseMenu.SetActive(!pauseMenu.activeSelf);
 	}
 }
---
+++ Assets/Scripts/Game/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Attached to the pause menu's 'quit' button - for now, 
+ * pauses the editor. */
+public class QuitGame : MonoBehaviour 
+{
+	private void OnMouseDown()
+	{
+		Debug.Break();
+	}
+}
